540a33f6f01bb6be60f7bcc5fca044973dc68dc3
squarecylinder <ziencck@gmail.com>
2023-07-28 09:30:29 -0400

    Apollo Server is now working.

--- StoryTowerWeb/server.js
+++ StoryTowerWeb/server.js
@@ -3,8 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const authMiddleware = require('./utils/auth');
 
-const { performWebScraping } = require('./src/scraping/webScraper');
-const {typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 
 const connection = require('./config/connection');
 
@@ -17,16 +16,11 @@ const server = new ApolloServer({
   context: authMiddleware,
 })
 
-const User = require('./models/User');
-const Story = require('./models/Story');
-const Comment = require('./models/Comment');
-
-
 // Middleware to handle parsing JSON data in request body
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV == 'production'){
+if (process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, '../StoryTower/build')))
 }
 
@@ -34,35 +28,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../StoryTower/'))
 })
 
-// Define your API endpoint
-app.get('/scrape', async (req, res) => {
-  try {
-    const scrapedData = await performWebScraping();
-    res.send(scrapedData);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
 // Start the server
 const startApolloServer = async () => {
   try {
+    // Await server.start() before applying the middleware
     await server.start();
     server.applyMiddleware({ app });
 
-    connection.once('open', () => {
+    // Establish MongoDB connection
+    connection.on('open', () => {
       console.log('Connected to MongoDB.');
-      console.log(`API Server running on ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      // Start Apollo Server after the MongoDB connection is open
+      app.listen(PORT, () => {
+        console.log(`API Server running at http://localhost:${PORT}${server.graphqlPath}`);
+      });
     });
   } catch (error) {
     console.error('Error starting Apollo server:', error);
   }
 };
 
-//startApolloServer();
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+startApolloServer();
