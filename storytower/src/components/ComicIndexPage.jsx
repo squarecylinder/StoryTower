3809807d3eac6094e6a13b76433f2f77ccc929ce
squarecylinder <ziencck@gmail.com>
2023-10-08 11:21:40 -0400

    Removed almost all of the useEffect()s in the ComicIndex Page. Added one to validate url Params so a user can type in /comics/genre/all/foo or # > lastPage and it will default to /all/1 or /all/lastPage. Could be over kill and can be taken out if we don't want to use any useEffects. Without it a user could happen to type /all/foo and then nothing will display on the page, and when they hit the next button it will go to /all/NaN and still not display. A user could just hit the home button or we make invalid params a 404?

--- storytower/src/components/ComicIndexPage.jsx
+++ storytower/src/components/ComicIndexPage.jsx
@@ -1,48 +1,31 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Link, useParams, useLocation } from 'react-router-dom';
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 import './ComicIndexPage.css';
-import client, { GET_STORIES, SEARCH_STORIES_BY_GENRE } from '../apolloClient';
+import { GET_STORIES, SEARCH_STORIES_BY_GENRE } from '../apolloClient';
 import LoadingScreen from './LoadingScreen';
-import debounce from 'lodash.debounce';
 
 const ComicIndexPage = () => {
-  const { page, genres }  = useParams();
-  const location = useLocation();
-  const [loading, setLoading] = useState(false);
-  const [formattedData, setFormattedData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(parseInt(page, 10) || 1); // Ensure currentPage is a number
-  const [currentGenre, setCurrentGenre] = useState('all');
-  const [totalStories, setTotalStories] = useState(0);
-  const [isLastPage, setIsLastPage] = useState(false);
-  console.log(currentGenre)
-  const debouncedFetchData = debounce(async (page, query) => {
-    try {
-      if (!loading) {
-        setLoading(true);
-        const offset = (page - 1) * 24;
-        const variables = query === GET_STORIES ? { offset, limit: 24 } : { genres: genres, offset, limit: 24}
-        const { data } = await client.query({
-          query,
-          variables,
-        });
-        const storyCatalog = data?.getStories|| data?.searchStoriesByGenre
-        setFormattedData(storyCatalog.data);
-        setTotalStories(storyCatalog.totalStories);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-    } finally {
-      setLoading(false);
+  const navigate = useNavigate()
+  const { page, genres } = useParams();
+  const { loading, error, data } = useQuery(genres == 'all' ? GET_STORIES : SEARCH_STORIES_BY_GENRE, {
+    variables: {
+      genres: genres || '',
+      offset: (page - 1) * 24,
+      limit: 24
     }
-  }, 500); // Adjust the delay (in milliseconds) as needed
-  
-  const fetchData = (page) => {
-    const query = location.pathname.includes('/all/')
-    ? GET_STORIES
-    : SEARCH_STORIES_BY_GENRE
-    genres != undefined ? setCurrentGenre(genres) : setCurrentGenre('all')
-    debouncedFetchData(page, query);
-  };
+  });
+  const totalStories = data?.getStories?.totalStories || data?.searchStoriesByGenre?.totalStories
+  const formattedData = data?.getStories?.data || data?.searchStoriesByGenre?.data
+
+  useEffect(() => {
+    if(isNaN(page) || page < 1 || (totalStories && page > Math.ceil(totalStories / 24))) {
+      console.log(page, ' is the page')
+      const validPage = totalStories ? Math.ceil(totalStories / 24) : 1;
+      navigate(`/comics/genre/${genres}/${validPage}`)
+    }
+  },[page, navigate, genres, totalStories])
+
 
   const renderStoryItem = ({ item }) => (
     <div key={item._id}>
@@ -50,37 +33,6 @@ const ComicIndexPage = () => {
       <p className="itemText">{item.title}</p>
     </div>
   );
-
-  const loadPage = useCallback(
-    async (pageNumber) => {
-      const lastPage = Math.max(1, Math.ceil(totalStories / 24));
-      if (isNaN(pageNumber) || pageNumber < 1) {
-        setCurrentPage(1);
-      } else if (pageNumber > lastPage) {
-        setCurrentPage(lastPage);
-      } else {
-        fetchData(pageNumber);
-        setCurrentPage(pageNumber);
-        setIsLastPage(pageNumber === lastPage);
-      }
-    },
-    [totalStories]
-  );
-
-  useEffect(() => {
-    // Fetch data whenever the page number changes
-    fetchData(currentPage);
-  }, [currentPage, genres]);
-
-  useEffect(() => {
-    // Update the currentPage when the component mounts
-    const parsedPage = parseInt(page, 10) || 1;
-    setCurrentPage(parsedPage);
-
-    // Fetch data based on the current location
-    const newPage = parseInt(location.pathname.split('/').pop(), 10) || 1;
-    loadPage(newPage);
-  }, [page, genres, loadPage, location]);
 
   return (
     <div className="contentContainer">
@@ -97,20 +49,17 @@ const ComicIndexPage = () => {
               ))}
             </div>
             <div className="paginationContainer">
-              <Link to={`/comics/genre/${currentGenre}/${Math.max(currentPage - 1, 1)}`} disabled={loading}>
+              <Link to={`/comics/genre/${genres}/${Math.max(page - 1, 1)}`} disabled={loading || parseInt(page) === 1}>
                 <button
                   className="paginationButton"
-                  onClick={() => loadPage(Math.max(currentPage - 1, 1))}
-                  disabled={currentPage === 1}
                 >
                   Previous
                 </button>
               </Link>
-              <Link to={`/comics/genre/${currentGenre}/${Math.min(currentPage + 1, Math.ceil(totalStories / 24))}`} disabled={loading || isLastPage}>
+              <Link to={`/comics/genre/${genres}/${Math.min(parseInt(page) + 1, Math.ceil(totalStories / 24))}
+              `} disabled={loading}>
                 <button
                   className="paginationButton"
-                  onClick={() => loadPage(Math.min(currentPage + 1, Math.ceil(totalStories / 24)))}
-                  disabled={isLastPage}
                 >
                   Next
                 </button>
