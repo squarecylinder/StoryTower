dfe180177afe6e6eccbeb6c38ab772d8964c7455
squarecylinder <ziencck@gmail.com>
2023-10-09 00:07:56 -0400

    Removed almost all useEffects

--- storytower/src/components/StoryDetails.jsx
+++ storytower/src/components/StoryDetails.jsx
@@ -1,8 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { GET_STORY, GET_CHAPTER_DETAILS } from '../apolloClient';
 import './StoryDetails.css';
+
+const getFormattedDate = (lastUpdated) => {
+    const formattedDate = new Date(parseInt(lastUpdated)).toISOString();
+    const [year, month, day] = formattedDate.split('T')[0].split('-');
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthName = monthNames[parseInt(month) - 1]; // Adjust for 0-based index
+    return `${monthName} ${day}, ${year}`;
+  };
 
 const StoryDetails = () => {
     const { storyId } = useParams();
@@ -13,25 +21,16 @@ const StoryDetails = () => {
     const [chapters, setChapters] = useState([]);
     const { loading: chapterLoading, error: chapterError, data: chapterData } = useQuery(GET_CHAPTER_DETAILS, {
         variables: { chapterIds: storyData?.story?.chapters.map(chapter => chapter._id) || [] },
-    });
-
-    useEffect(() => {
-        if (!chapterLoading && !chapterError && chapterData && chapterData.chapters) {
-            setChapters(chapterData.chapters);
+        onCompleted: () => {
+            setChapters(chapterData.chapters)
         }
-    }, [chapterLoading, chapterError, chapterData]);
+    });
 
     if (storyLoading || chapterLoading) return <div>Loading...</div>;
     if (storyError) return <div>Error: {storyError.message}</div>;
     if (chapterError) return <div>Error: {chapterError.message}</div>;
 
     const { story } = storyData;
-    const formattedDate = new Date(parseInt(story.lastUpdated)).toISOString();
-    const [year, month, day] = formattedDate.split('T')[0].split('-');
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const monthName = monthNames[parseInt(month) - 1]; // Adjust for 0-based index
-
-    const formattedDateString = `${monthName} ${day}, ${year}`;
 
     return (
         <div className="story-details">
@@ -40,7 +39,7 @@ const StoryDetails = () => {
                     <h2>{story.title}</h2>
                     <img src={story.coverArt} alt="Cover Art" className="cover-art" />
                     <p>Rating: {story.rating}</p>
-                    <p>Last Updated: {formattedDateString}</p>
+                    <p>Last Updated: {getFormattedDate(story.lastUpdated)}</p>
                     <p>Chapter Count: {story.chapterCount}</p>
                     <p>Synopsis: {story.synopsis}</p>
                     <p>Genres: {story.genres.join(', ')}</p>
@@ -51,17 +50,17 @@ const StoryDetails = () => {
                             <h3>Chapter Titles</h3>
                             <div className="chapter-cards">
                                 <div className="chapter-links">
-                                    <Link to={`/chapter/${chapters[0]._id}`} state={chapters[0]} className="chapter-card">
+                                    <Link to={`/chapter/${chapters[0]._id}`}className="chapter-card">
                                         First Chapter
                                     </Link>
-                                    <Link to={`/chapter/${chapters[chapters.length - 1]._id}`} state={chapters[chapters.length - 1]} className="chapter-card">
+                                    <Link to={`/chapter/${chapters[chapters.length - 1]._id}`} className="chapter-card">
                                         Latest Chapter
                                     </Link>
                                 </div>
                                 {chapters.map((chapter, index) => (
-                                    <Link to={`/chapter/${chapter._id}`} state={chapter}>
+                                    <Link to={`/chapter/${chapter._id}`}>
                                         <div key={index} className='chapter-card'>
-                                            {chapter.title}
+                                            {chapter.title} {getFormattedDate(chapter.uploaded)}
                                         </div>
                                     </Link>
                                 ))}
