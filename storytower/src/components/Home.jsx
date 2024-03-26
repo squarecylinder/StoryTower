ec0545ff9dad3ce283eaa033db2f08ba87d9f36e
squarecylinder <ziencck@gmail.com>
2024-03-26 13:48:00 -0400

    Adding a results page instead of showing just the first result. Added tailwind, will start ripping out custom css eventually

--- storytower/src/components/Home.jsx
+++ storytower/src/components/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome to StoryTower!</h1>
+      <p>A catalog of manwha, manga, and manhua scrapped from Asura scans and more in the future!</p>
+      <p>The point of this website for me, was to have a reliable way to share stories with friends without having to worry if the website was shady.</p>
+      <p>This site has basic features currently for viewing stories and saving them as bookmarks in your account. Working on how to rate and comment on stories and chapters.</p>
       <Link to="/comics/genre/All/1">Read All Comics</Link>
     </div>
   );
---
+++ storytower/src/components/Results.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useParams, Link, useLocation} from 'react-router-dom';
+
+// Making this a reusable component for like the index page, genre page, bookmarks
+// seems like a good idea
+
+const Results = () =>{
+    const location = useLocation();
+    const { stories } = location.state
+    const hasResults = stories.length > 0 ? true : false
+
+    const renderStoryItem = ({ item }) => (
+        <div className="max-w-48" key={item._id}>
+          <img className=" max-h-54" src={item.coverArt} alt="Cover Art"/>
+          <p>{item.title}</p>
+        </div>
+      );
+
+    return (
+        <div className="pt-6">
+            {
+            hasResults ? (
+                <div className="grid gap-2 grid-cols-8">
+                    {stories.map((item) => 
+                        <Link to={`/story/${item._id}`} state={{ story: item }} key={item._id}>
+                        <div className="bg-cyan-700 p-4">
+                            {renderStoryItem({ item })}
+                            </div>
+                        </Link>
+                            )}
+                </div>
+            ) : (<> <h1>Oops, looks like there are no results! </h1></>)
+            }
+        </div>
+    )
+}
+
+export default Results
